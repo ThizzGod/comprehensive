286b90d8e31fbaa9330834ced9f9de30352919d9
jgac72 <jgac72@gmail.com>
2025-12-02 17:59:47 -0700

    java docs and tester class

--- GrammarReader.java
+++ GrammarReader.java
@@ -2,18 +2,32 @@ package comprehensive;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Random;
 
+/**
+ * Reads a grammar file and helps create random phrases.
+ * This handles non terminals like object and terminals like words.
+ * 
+ * @author Max Barker & Josi Gac
+ * @version 12/2/25
+ */
 public class GrammarReader {
-	public GrammarReader() {
-
-	}
 	
+	/**
+	 * Default constructor.
+	 */
+	public GrammarReader() {}
+	
+	/**
+	 * Reads the starting phrase from the grammar file.
+	 * 
+	 * @param filePath path to the grammar file
+	 * @return the first phrase to start generating from
+	 */
 	public static String getPhrase(String filePath) {
 		BufferedReader buff;
 		StringBuilder builder = new StringBuilder();
@@ -31,6 +45,12 @@ public class GrammarReader {
 		return builder.toString();
 	}
 	
+	/**
+	 * Builds a map of all non terminals in the grammar.
+	 * 
+	 * @param filePath filePath path to the grammar file
+	 * @return a map where the key is a non terminal and the value is its list of options
+	 */
 	public static HashMap<String, ArrayList<String>> buildGrammarMap(String filePath) {
 		BufferedReader buff;
 		HashMap<String, ArrayList<String>> map = new HashMap<String, ArrayList<String>>();
@@ -58,6 +78,13 @@ public class GrammarReader {
 	return map;	
 	}
 	
+	/**
+	 * Recursively creates a random phrase by replacing non terminals.
+	 * 
+	 * @param phrase the current phrase
+	 * @param grammarMap map of all non terminals and their options
+	 * @return a fully written random phrase
+	 */
 	public static String createNewPhrase(String phrase, HashMap<String, ArrayList<String>> grammarMap) {
 		Random rng = new Random();
 		StringBuilder phraseBuilder = new StringBuilder();
@@ -79,5 +106,4 @@ public class GrammarReader {
 		}
 		return phraseBuilder.toString();
 	}
-	
 }
--- RandomPhraseGenerator.java
+++ RandomPhraseGenerator.java
@@ -4,8 +4,19 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Random;
 
+/**
+ * Main class to generate random phrases from a grammar file.
+ * 
+ * @author Max Barker & Josi Gac
+ * @version 12/2/25
+ */
 public class RandomPhraseGenerator {
 
+	/**
+	 * Reads the grammar and prints random phrases.
+	 * 
+	 * @param args args[0] = grammar file path and args[1] = number of phrases.
+	 */
 	public static void main(String[] args) {
 		Random rng = new Random();
 		String phrase = GrammarReader.getPhrase(args[0]);
